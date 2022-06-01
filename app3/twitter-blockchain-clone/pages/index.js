95ea925f0250eb9dd4099568fbcce5f0d4d01468
queen mudau <shudaumudau@gmail.com>
2022-06-01 12:48:36 +0200

    twitterapp with next.js and tailwind

---
+++ app3/twitter-blockchain-clone/pages/index.js
@@ -0,0 +1,28 @@
+
+import { useContext } from 'react'
+import { TwitterContext } from '../context/TwitterContext'
+import Feed from '../components/home/Feed'
+import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
+import metamaskLogo from '../assets/metamask.png'
+import errorImg from '../assets/error.png'
+import Image from 'next/image'
+
+const style = {
+  wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  content: `max-w-[1400px] w-2/3 flex justify-between`,
+  loginContainer: `w-full h-full flex flex-col justify-center items-center pb-48`,
+  walletConnectButton: `text-2xl text-black bg-white font-bold mb-[-3rem] mt-[3rem] px-6 py-4 rounded-full cursor-pointer hover:bg-[#d7dbdc]`,
+  loginContent: `text-3xl font-bold text-center mt-24`,
+}
+
+const Home = () => {
+  const { appStatus, connectWallet } = useContext(TwitterContext)
+
+  const app = (status = appStatus) => {
+    switch (status) {
+      case 'connected':
+        return userLoggedIn
+
+      case 'notConnected':
+        return noUserFound
